da86db45bcdfd8c9821450a4ba0552e9b615f8eb
tangjun114 <472181201@qq.com>
2013-11-03 01:26:56 +0800

    config

---
+++ code/Fuego/WebRoot/jsp/gasStationCheckStatus.jsp
@@ -0,0 +1,87 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8" isELIgnored="false"%>
+ <%@ taglib uri="http://struts.apache.org/tags-html" prefix="html"%>
+ <%@ taglib uri="http://struts.apache.org/tags-bean" prefix="bean"%>
+  <%@ taglib uri="http://struts.apache.org/tags-logic" prefix="logic"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html>
+ 	<jsp:include page="/jsp/cbb/includeCSS.jsp"/>
+<body>
+ 
+     	<jsp:include page="/jsp/cbb/header.jsp"/>
+    
+		
+            
+		
+		
+
+		
+		<div id="content">
+			<div id="content-header">
+				<h1><font  face="微软雅黑">盘点情况统计</font></h1>
+
+			</div>
+			<div id="breadcrumb">
+				<a href="#" title="Go to Home" class="tip-bottom"><i class="icon-home"></i>主页</a>
+				<a href="#" class="current">盘点管理</a>
+				<a href="#" class="current">盘点情况统计</a>
+			</div>
+			<div class="container-fluid">
+				<div class="row-fluid">
+					<div class="span12">
+						
+					
+						<div class="widget-box">
+						
+							<div class="widget-title">							
+								<h5>${initBo.title}</h5>		
+							</div>
+						
+							<div class="widget-content nopadding">
+								<table class="table table-bordered data-table">
+									<thead>
+										<tr>
+										<th>加油站</th>
+										<th>盘点状态</th>	
+										<th>查看</th>	
+										</tr>
+									</thead>
+									<tbody>
+									<c:forEach var="i" items= "${checkPlan.transInfo.childTransList}"> 		
+										<tr>										
+											
+ 											<td style="text-align:center">${i.transInfo.handleUser}</td>
+											<td style="text-align:center">${i.transInfo.state}</td>
+											<td style="text-align:center"><a href="<%=request.getContextPath()%>/GasStationCheckStatusEnsureInit.do?transID=${i.transInfo.transID}" title="查看">查看</a></td>
+										</tr>
+									</c:forEach>		
+										
+									 
+									</tbody>
+								
+								</table> 
+								
+							</div>
+							
+						</div>
+						
+						<form action="<%=request.getContextPath()%>/GasStationCheckStatus.do" method="post" class="form-horizontal" >
+						<button type="submit" class="btn btn-primary" name="submit" value="confirm">确定</button>						
+						</form>
+					</div>
+				</div>
+							<div class="row-fluid">
+					<div id="footer" class="span12">
+						2013  Copyright Reserved by Tinder.
+					</div>
+				</div>	
+				
+			
+			</div>
+		</div>
+					
+ 
+	</body>
+</html>
--- code/Fuego/src/cn/tinder/fuego/webservice/struts/action/index/IndexAction.java
+++ code/Fuego/src/cn/tinder/fuego/webservice/struts/action/index/IndexAction.java
@@ -1,5 +1,7 @@
 package cn.tinder.fuego.webservice.struts.action.index;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -10,6 +12,17 @@ import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
+import cn.tinder.fuego.service.ServiceContext;
+import cn.tinder.fuego.service.TransactionService;
+import cn.tinder.fuego.service.exception.ServiceException;
+import cn.tinder.fuego.util.constant.LogKeyConst;
+import cn.tinder.fuego.webservice.struts.bo.base.MenuTreeBo;
+import cn.tinder.fuego.webservice.struts.bo.base.SystemUserBo;
+import cn.tinder.fuego.webservice.struts.bo.trans.TransactionBaseInfoBo;
+import cn.tinder.fuego.webservice.struts.constant.PageNameConst;
+import cn.tinder.fuego.webservice.struts.constant.ParameterConst;
+import cn.tinder.fuego.webservice.struts.constant.RspBoNameConst;
+
 /**
  * @author Nan Bowen
  * @function:LoginAction
@@ -17,18 +30,43 @@ import org.apache.struts.action.ActionMapping;
 public class IndexAction extends Action
 {
 	private static final Log log = LogFactory.getLog(IndexAction.class);
+	private TransactionService transactionService =ServiceContext.getInstance().getTransactionService();
 
 	@Override
 	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception
 	{
-		log.info("[Action]Get into IndexInit.do");
+        log.info(LogKeyConst.INPUT_ACTION);
+        
+    	String nextPage = null;
+    	try
+    	{
+    		nextPage = handle(form,request);
+		} 
+    	catch(ServiceException e)
+    	{
+    		log.warn("opration failed",e);
+    		request.setAttribute(RspBoNameConst.OPERATE_EXCEPION, e.getMessage());
+			nextPage = PageNameConst.ERROR_PAGE; 
+    	}
+    	catch (Exception e)
+		{
+			log.error("system error",e);
+			nextPage = PageNameConst.SYSTEM_ERROR_PAGE; 
+		}
 
-		// Mapping
-		String pageName = "Index";
+        log.info(LogKeyConst.NEXT_PAGE+nextPage);
+        return mapping.findForward(nextPage);	
 
-		log.info("[Jump]:NextPage:" + pageName);
-		return mapping.findForward(pageName);
+	}
+	private String handle(ActionForm form,HttpServletRequest request)
+	{
+		String nextPage = PageNameConst.INDEX_INIT_ACTION;
+		 
+		SystemUserBo user = (SystemUserBo) request.getSession().getAttribute(RspBoNameConst.SYSTEM_USER);
 
+		String transID = request.getParameter(ParameterConst.PLAN_TRANS_ID);
+		transactionService.deletePlanByTransID(user.getUserID(), transID);
+		return nextPage;
 	}
 
 }
