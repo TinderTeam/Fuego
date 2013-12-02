e6eec829ffe00ff21b801f9e2a6b862f54eea34a
tangjun114 <472181201@qq.com>
2013-12-03 01:57:31 +0800

    fix  merge wrong script by zlc

--- script/install/StaticData/trans_event_type.sql
+++ script/install/StaticData/trans_event_type.sql
@@ -3,7 +3,7 @@ USE `feugodb`;
  
 INSERT INTO `trans_event_type` VALUES ('A',5,0);
 
-INSERT INTO `trans_event_type` VALUES ('D',3,0);
+INSERT INTO `trans_event_type` VALUES ('D',5,0);
 
 INSERT INTO `trans_event_type` VALUES ('R',3,0);
 
@@ -11,7 +11,7 @@ INSERT INTO `trans_event_type` VALUES ('C',2,0);
 
 INSERT INTO `trans_event_type` VALUES ('E',2,0);
 
-INSERT INTO `trans_event_type` VALUES ('P',3,0);
+INSERT INTO `trans_event_type` VALUES ('P',5,0);
 
  
  
